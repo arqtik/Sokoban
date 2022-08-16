618aac79cd6f9cabd32050d12a901a17ea474608
arqtik <martynascamets1@gmail.com>
2022-08-16 12:58:46 +0200

    Update SokobanExtended.js
    Added some comments and formatting

--- SokobanExtended.js
+++ SokobanExtended.js
@@ -1,11 +1,12 @@
 document.onkeydown = checkInput;
 
 let gameEnd = false;
-let tileClassUnderPlayer = Tiles.Space;
+let tileClassUnderPlayer = Tiles.Space; // Remembers which tile is under player so that it can be replace back when player moves
 let amountOfGoals = 0;
 
-function checkInput(input){
-    if(input.repeat || gameEnd) {return}
+function checkInput(input) {
+    // Disable input if holding key or game over
+    if (input.repeat || gameEnd) { return }
 
     /*
         37: Left
@@ -13,7 +14,7 @@ function checkInput(input){
         39: Right
         40: Down
     */
-    switch(input.keyCode){
+    switch (input.keyCode) {
         case 37:
             input.preventDefault();
             movePlayer(-1, 0)
@@ -33,11 +34,12 @@ function checkInput(input){
     }
 }
 
-function movePlayer(x, y){
+// Move player in direction
+function movePlayer(x, y) {
     let currentPlayerTile = getPlayerElement();
     let playerPos = getVector2FromTileId(currentPlayerTile.id);
     let nextMoveTile = getTileByCoordinates(playerPos[0] + x, playerPos[1] + y);
-    
+
     if (nextMoveTile.className === Tiles.Wall) {
         return;
     }
@@ -57,12 +59,12 @@ function movePlayer(x, y){
     checkWinCondition();
 }
 
-function tryMoveBlock(blockX, blockY, dirX, dirY){
+function tryMoveBlock(blockX, blockY, dirX, dirY) {
     let tileInDirection = getTileByCoordinates(blockX + dirX, blockY + dirY);
 
-    if(tileInDirection.className === Tiles.Wall ||
+    if (tileInDirection.className === Tiles.Wall ||
         tileInDirection.className === Entities.Block ||
-        tileInDirection.className === Entities.BlockDone){
+        tileInDirection.className === Entities.BlockDone) {
         return false;
     }
 
@@ -83,7 +85,7 @@ function tryMoveBlock(blockX, blockY, dirX, dirY){
     return true;
 }
 
-function checkWinCondition(){
+function checkWinCondition() {
     let doneBlockTiles = document.getElementsByClassName(Entities.BlockDone);
     doneBlockTiles = doneBlockTiles.length;
 
@@ -94,7 +96,7 @@ function checkWinCondition(){
     }
 }
 
-function getVector2FromTileId(tileId){
+function getVector2FromTileId(tileId) {
     tileId = tileId.slice(1, tileId.length)
     let vector2 = tileId.split('y');
     vector2[0] = parseInt(vector2[0]);
@@ -102,19 +104,19 @@ function getVector2FromTileId(tileId){
     return vector2;
 }
 
-function getPlayerElement(){
+function getPlayerElement() {
     return document.getElementsByClassName(Entities.Character)[0];
 }
 
 function generateTiles(map) {
-    let main = document.getElementById('tile-grid'); 
+    let main = document.getElementById('tile-grid');
 
     for (let i = 0; i < map.height; i++) {
         for (let j = 0; j < map.width; j++) {
             let tile = document.createElement('div');
             tile.id = 'x' + j + 'y' + i;
-            
-            switch(map.mapGrid[i][j][0]){
+
+            switch (map.mapGrid[i][j][0]) {
                 case " ":
                     tile.className = Tiles.Space;
                     break;
@@ -138,7 +140,7 @@ function generateTiles(map) {
     }
 }
 
-function getTileByCoordinates(x, y){
+function getTileByCoordinates(x, y) {
     let tileId = 'x' + x + 'y' + y;
     return document.getElementById(tileId);
 }
